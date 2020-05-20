c11637702c47917c5669eedae0ca8eabec5cb76c
vvlookman <vvlookman@gmail.com>
2020-05-20 18:15:24 +0800

    债券基金分析实验：修正没有计算无风险资产收益率的错误

--- subject-experiment/bond-funds-analysis/sharp-ratio.py
+++ subject-experiment/bond-funds-analysis/sharp-ratio.py
@@ -5,5 +5,6 @@ df = pd.read_csv('ac-worth-from-2017/002138.csv')
 df['daily_return'] = df['worth'].pct_change()
 
 days = df['date'].count()
-sharp_ratio = np.sqrt( days / 3.347 ) * df['daily_return'].mean() / df['daily_return'].std()
+return_days = days / 3.347
+sharp_ratio = ( return_days * df['daily_return'].mean() - 0.03) / ( df['daily_return'].std() * np.sqrt(return_days) )
 print(days, sharp_ratio)
